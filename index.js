62226474a448682a37a52feb5e5bcac991396a53
theagentpenguin <gunaseelanabcd@gmail.com>
2025-06-12 07:06:57 +0530

    created a new branch, made some changes as per project requirement in roadmap.

--- index.js
+++ index.js
@@ -5,46 +5,45 @@ const TASKS_FILE = 'tasks.json';
 let args = process.argv.slice(2);
 //console.log(args[1]);
 let tasks = [];
+let emptyArray = [];
 let taskObject;
+let currentTime = new Date();
 
 if (args[0].toLowerCase() === "add" ){
     addTask(args);
 }
-// if (args[0].toLowerCase() === "update" ){
-//     updateTask(args[1]);
-// }
+if (args[0].toLowerCase() === "update" ){
+    let data = [args[1],args[2]];
+    updateTask(data);
+}
+
 // if (args[0].toLowerCase() === "delete" ){
 //     deleteTask(args[1]);
 // }
+
 if (args[0].toLowerCase() === "list" ){
-    const fileContent = fs.readFileSync('data.json','utf8');
-    let existingTasks = JSON.parse(fileContent);
-    console.log(existingTasks);
+    listTask();
 }
-function addTask(data){
 
-    console.log("Added task:" + chalk.yellow(data[1]));
-    
-    if(data.length !==2){
-        console.log("Enter the data properly");
-        return;
+function checkFile(){
+    if(fs.existsSync('data.json')){
+        return true;
     }
-    const [command, taskInfo] = data;
-    if(command === "add"){
-        const randomId = Math.floor(Math.random()*1000000);
-        taskObject = {
-            desc: taskInfo,
-            id: randomId.toString()
-        }
-    };
-    tasks.push(taskObject);
-    
-    loadTasks();
-    return;
-
 }
-//console.log(tasks);
+
+function isFileEmpty(){
+    /* This function checks if the json file is empty, important to prevent runtime issues */
+    const fileData = fs.readFileSync('data.json','utf8');
+    if(fileData.length === 0){
+        return true;
+    }
+}
+
 function loadTasks(){
+    /* To check if the file is empty and if empty, an empty array will be pushed into the file first */
+    if(isFileEmpty()){
+        fs.writeFileSync('data.json',JSON.stringify(emptyArray,null,2),'utf8');
+    }
     if(checkFile()){
         const fileContent = fs.readFileSync('data.json','utf8');
         let existingTasks = JSON.parse(fileContent);
@@ -57,14 +56,57 @@ function loadTasks(){
     
 }
 
-function checkFile(){
-    if(fs.existsSync('data.json')){
-        return true;
+function listTask(){
+    if(isFileEmpty()){
+        console.log("The list is empty, start adding!");
     }
+    else if(checkFile()){
+        
+        const fileContent = fs.readFileSync('data.json','utf8');
+        let existingTasks = JSON.parse(fileContent);
+        console.log(existingTasks);
+    }
+    else{
+        console.log("There is no list yet. Start to add tasks");
+    }    
 }
-// function updateTask(data){
-//     console.log("Updated task:" + data);
-// }
+
+function addTask(data){
+
+    console.log("Added task:" + chalk.yellow(data[1]));
+    
+    if(data.length !==2){
+        console.log("Enter the data properly");
+        return;
+    }
+    const [command, taskInfo] = data;
+    if(command === "add"){
+        const randomId = Math.floor(Math.random()*100);
+        taskObject = {
+            desc: taskInfo,
+            id: randomId.toString(),
+            status: "todo",
+            createdAt: currentTime.toLocaleString(),
+            updatedAt: ""
+        }
+    };
+    
+    tasks.push(taskObject);
+    
+    loadTasks();
+    return;
+
+}
+//console.log(tasks);
+
+
+
+function updateTask(data){
+    console.log("Updated task:" + chalk.green(data[1]));
+    
+}
+
+
 
 // function deleteTask(data){
 //     console.log("Deleted task:" + data);
